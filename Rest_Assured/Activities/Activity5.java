636a38f4635cd57c8de45d30bc04d8e48d998d4c
Anjali2692 <anjali.dasari8@gmail.com>
2026-08-12 00:23:24 +0530

    Add files via upload

---
+++ Rest_Assured/Activities/Activity5.java
@@ -0,0 +1,12 @@
+public class ProviderTest {
+    // Other statements
+
+    @State("POST Request")
+    public void state1() {}
+
+    @State("GET Request")
+    public void state2() {}
+
+    @State("DELETE Request")
+    public void state3() {}
+}    
