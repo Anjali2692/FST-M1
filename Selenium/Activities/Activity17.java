e169164802de29714d0cf9ba7d96e3353ff61408
Anjali2692 <anjali.dasari8@gmail.com>
2026-09-08 22:20:53 +0530

    Update Activity17.java

--- Selenium/Activities/Activity17.java
+++ Selenium/Activities/Activity17.java
@@ -1,50 +1,44 @@
+package activity;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.time.Duration;
 import java.util.List;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebDriver;
 
 public class Activity17 {
-    public static void main(String[] args) {
-        // Create a new instance of the Firefox driver
-        WebDriver driver = new FirefoxDriver();
 
-        // Open the page
-        driver.get("https://training-support.net/webelements/selects");
-        // Print the title of the page
-        System.out.println("Page title: " + driver.getTitle());
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+        driver.get(" https://training-support.net/webelements/selects");
+        driver.manage().window().maximize();
+        System.out.println("Page Title: " + driver.getTitle());
 
-        // Find the dropdown
-        WebElement selectElement = driver.findElement(By.cssSelector("select.h-80"));
-        // Pass the WebElement to the Select object
-        Select multiSelect = new Select(selectElement);
-
-        // Select "HTML" using visible text
+        WebElement multiSelectElement = driver.findElement(By.xpath("(//select)[2]"));
+        Select multiSelect = new Select(multiSelectElement);
+        if (multiSelect.isMultiple()) {
         multiSelect.selectByVisibleText("HTML");
-        // Select 4th, 5th, and 6th index options
-        for (int i = 3; i <= 5; i++) {
-            multiSelect.selectByIndex(i);
-        }
-        // Select "Node" using value attribute
+        multiSelect.selectByIndex(3);
+        multiSelect.selectByIndex(4);
+        multiSelect.selectByIndex(5);
         multiSelect.selectByValue("nodejs");
-        // Print the selected options
+        multiSelect.deselectByIndex(5);
+        System.out.println("Multi-select operations completed successfully.");
+        } else {
+            System.out.println("This dropdown does not support multiple selection.");
+        }
+        
         List<WebElement> selectedOptions = multiSelect.getAllSelectedOptions();
-        System.out.println("Selected options are: ");
         for (WebElement option : selectedOptions) {
             System.out.println(option.getText());
         }
-
-        // Deselect the 5th index option
-        multiSelect.deselectByIndex(4);
-        // Print the selected options
-        selectedOptions = multiSelect.getAllSelectedOptions();
-        System.out.println("Selected options are: ");
-        for (WebElement option : selectedOptions) {
-            System.out.println(option.getText());
-        }
-
-        // Close the browser
         driver.quit();
     }
 }
