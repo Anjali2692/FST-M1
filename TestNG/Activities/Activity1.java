d8bb8e25883ee98b1130d023549d70ba3b7be916
Anjali2692 <anjali.dasari8@gmail.com>
2026-09-08 22:39:42 +0530

    Update Activity1.java

--- TestNG/Activities/Activity1.java
+++ TestNG/Activities/Activity1.java
@@ -1,43 +1,39 @@
+package Activity;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
- 
-public class Activity1 {
-    WebDriver driver;
-    
-    // Setup function
-    @BeforeClass
-    public void setUp() {
-        // Initialize Firefox driver
-        driver = new FirefoxDriver();
-        // Open the page
-        driver.get("https://training-support.net");
-    }
-    
-    // Test function
-    @Test(priority = 1)
-    public void homePageTest() {
-        // Assert page title
-        Assert.assertEquals(driver.getTitle(), "Training Support");
-        
-        // Find and click the About page link
-        driver.findElement(By.linkText("About Us")).click();
-    }
-    
-    @Test(priority = 2)
-    public void aboutPageTest() {
-        // Assert page title
-        Assert.assertEquals(driver.getTitle(), "About Training Support");
-    }
-    
-    // Teardown function
-    @AfterClass
-    public void tearDown() {
-        // Close the browser
-        driver.quit();
-    }
+
+public class Activity1  {
+
+	WebDriver driver;
+	
+  @BeforeTest
+  public void setUp() {
+
+      driver = new FirefoxDriver();
+      driver.manage().window().maximize();
+      driver.get("https://training-support.net");
+  }
+  
+  @Test
+  public void verifyTitle() {
+
+      String title = driver.getTitle();
+      System.out.println(title);
+      Assert.assertEquals(title, "Training Support");
+      driver.findElement(By.linkText("About Us")).click();
+      String aboutTitle = driver.getTitle();
+      System.out.println(aboutTitle);
+      Assert.assertEquals(aboutTitle, "About Training Support");
+  }
+  
+  @AfterTest
+  public void tearDown() {
+      driver.quit();
+  }
 }
