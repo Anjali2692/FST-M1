536f598d7a7a9a5e8e5b788020afbff64230b3ba
Anjali2692 <anjali.dasari8@gmail.com>
2026-09-08 22:44:10 +0530

    Update Activity7.java

--- TestNG/Activities/Activity7.java
+++ TestNG/Activities/Activity7.java
@@ -1,3 +1,5 @@
+package Activities;
+
 import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,52 +12,44 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
- 
+
 public class Activity7 {
     WebDriver driver;
     WebDriverWait wait;
- 
+
     @BeforeClass
     public void setUp() {
-        // Initialize driver
         driver = new FirefoxDriver();
-        // Initialize wait
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        // Open the page
         driver.get("https://training-support.net/webelements/login-form");
     }
- 
+
     @DataProvider(name = "Credentials")
     public static Object[][] creds() {
         return new Object[][] { 
-            { "admin1", "password1", "Invalid Credentials" },
-            { "wrongAdmin", "wrongPassword", "Invalid Credentials" }
+            { "admin1", "password1", "Invalid credentials" },
+            { "wrongAdmin", "wrongPassword", "Invalid credentials" }
         };
     }
- 
+
     @Test(dataProvider = "Credentials")
     public void loginTest(String username, String password, String expectedMessage) {
-        // Find the input fields and the login button
         WebElement usernameField = driver.findElement(By.id("username"));
         WebElement passwordField = driver.findElement(By.id("password"));
         WebElement loginButton = driver.findElement(By.xpath("//button[text()='Submit']"));
- 
-        // Clear the input fields
+
         usernameField.clear();
         passwordField.clear();
-        // Enter the credentials and click Log in
         usernameField.sendKeys(username);
         passwordField.sendKeys(password);
         loginButton.click();
- 
-        // Assert login message
+
         String loginMessage = driver.findElement(By.id("subheading")).getText();
         Assert.assertEquals(loginMessage, expectedMessage);
     }
- 
+
     @AfterClass
     public void tearDown() {
-        // Close the browser
-        driver.quit();
+         driver.quit();
     }
 }
