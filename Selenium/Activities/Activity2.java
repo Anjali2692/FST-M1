4874b526d0d5c98168416480a792f432dda3c9c9
Anjali2692 <anjali.dasari8@gmail.com>
2026-09-08 22:22:29 +0530

    Update Activity2.java

--- Selenium/Activities/Activity2.java
+++ Selenium/Activities/Activity2.java
@@ -1,29 +1,23 @@
+package activity;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Activity2 {
-    public static void main(String[] args) {
-        // Initialize the Firefox driver
+
+	public static void main(String[] args) {
         WebDriver driver = new FirefoxDriver();
 
-        // Open the page
-        driver.get("https://training-support.net/webelements/login-form");
-        // Print the title of the page
-        System.out.println("Page title: " + driver.getTitle());
+        try {
+              driver.get("https://training-support.net/webelements/login-form/");
+              System.out.println("Page Title: " + driver.getTitle());
+              driver.findElement(By.id("username")).sendKeys("admin");
+              driver.findElement(By.id("password")).sendKeys("password");
+              driver.findElement(By.xpath("//button[text()='Log in']")).click();
 
-        // Find the username field and enter the username
-        driver.findElement(By.id("username")).sendKeys("admin");
-        // Find the password field and enter the password
-        driver.findElement(By.id("password")).sendKeys("password");
-        // Find the login button and click it
-        driver.findElement(By.xpath("//button[text()='Submit']")).click();
-
-        // Print the confirmation message
-        String message = driver.findElement(By.tagName("h1")).getText();
-        System.out.println(message);
-
-        // Close the browser
-        driver.quit();
+        } finally {
+            driver.quit();
+        }
     }
 }
