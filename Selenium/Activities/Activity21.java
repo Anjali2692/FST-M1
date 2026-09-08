a8abd54ff5dfb7d2084500a2153645536ab3c235
Anjali2692 <anjali.dasari8@gmail.com>
2026-09-08 22:23:37 +0530

    Update Activity21.java

--- Selenium/Activities/Activity21.java
+++ Selenium/Activities/Activity21.java
@@ -1,54 +1,50 @@
-import java.time.Duration;
+package activity;
+
+import java.util.ArrayList;
+import java.util.Set;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Activity21 {
-    public static void main(String[] args) {
-        // Create a new instance of the Firefox driver
-        WebDriver driver = new FirefoxDriver();
-        // Create the Wait object
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        // Open the page
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new FirefoxDriver();
         driver.get("https://training-support.net/webelements/tabs");
-        // Print the title of the page
-        System.out.println("Page title: " + driver.getTitle());
-        // Print the handle of the parent window
-        System.out.println("Current tab: " + driver.getWindowHandle());
-
-        // Find button to open new tab
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Open A New Tab']"))).click();
-        // Wait for the second tab to open
-        wait.until(ExpectedConditions.numberOfWindowsToBe(2));
-        // Print all window handles
-        System.out.println("Currently open windows: " + driver.getWindowHandles());
-
-        // Switch focus to the latest tab
-        for (String handle : driver.getWindowHandles()) {
-            driver.switchTo().window(handle);
+        driver.manage().window().maximize();
+        System.out.println("Page Title: " + driver.getTitle());
+        
+        driver.findElement(By.xpath("//button[contains(text(),'Open A New Tab')]")).click();
+        Thread.sleep(2000);
+        
+        Set<String> handles = driver.getWindowHandles();
+        System.out.println("Window Handles:");
+        for (String handle : handles) {
+            System.out.println(handle);
         }
 
-        // Wait for the new page to load
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(), 'Another One')]")));
-        // Print the handle of the current tab
-        System.out.println("Current tab: " + driver.getWindowHandle());
-        // Print the title and heading of the new page
-        System.out.println("New Page title: " + driver.getTitle());
-        System.out.println("New Page message: " + driver.findElement(By.cssSelector("h2.mt-5")).getText());
-        // Find and click the button on page to open another tab
-        driver.findElement(By.xpath("//button[contains(text(), 'Another One')]")).click();
+        ArrayList<String> tabs = new ArrayList<>(handles);
+        driver.switchTo().window(tabs.get(1));
+        System.out.println("Second Tab Title: " + driver.getTitle());
 
-        // Wait for new tab to open
-        wait.until(ExpectedConditions.numberOfWindowsToBe(3));
-        // Switch focus
-        for (String handle : driver.getWindowHandles()) {
-            driver.switchTo().window(handle);
+        WebElement message = driver.findElement(By.tagName("h1"));
+        System.out.println("Message: " + message.getText());
+        driver.findElement(By.xpath("//button[contains(text(),'Open Another One')]")).click();
+        Thread.sleep(2000);
+        handles = driver.getWindowHandles();
+        System.out.println("\nUpdated Window Handles:");
+        for (String handle : handles) {
+            System.out.println(handle);
         }
 
-        // Close the browser
+        tabs = new ArrayList<>(handles);
+        driver.switchTo().window(tabs.get(2));
+        System.out.println("Third Tab Title: " + driver.getTitle());
+        message = driver.findElement(By.tagName("h1"));
+        System.out.println("Message: " + message.getText());
+        
         driver.quit();
     }
 }
