01ba385c0dc0886b156a5ac52c601b85b98f1562
Anjali2692 <anjali.dasari8@gmail.com>
2026-09-08 22:40:37 +0530

    Update Activity11.java

--- TestNG/Activities/Activity11.java
+++ TestNG/Activities/Activity11.java
@@ -1,11 +1,14 @@
+package Activities;
+
 import static org.testng.Assert.assertEquals;
+
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.Row;
@@ -20,127 +23,115 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
- 
+
 public class Activity11 {
-	WebDriver driver;
-	WebDriverWait wait;
- 
-	@BeforeClass
-	public void beforeClass() {
-		driver = new FirefoxDriver();
-		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
- 
-		// Open browser
-		driver.get("https://training-support.net/webelements/simple-form");
-	}
- 
-	public static List<List<String>> readExcel(String filePath) {
-		// Creating the base list
-		List<List<String>> data = new ArrayList<>();
- 
-		// Create the workbook object
-		Workbook excelFile;
-		try {
-			excelFile = new XSSFWorkbook(new FileInputStream(filePath));
- 
-			// Select the sheet from the workbook
-			Sheet sheet1 = excelFile.getSheetAt(0);
- 
-			// Iterate through the rows in the sheet
-			for (Row rows : sheet1) {
-				// To skip the first row
-				if (rows.getRowNum() == 0) {
-					continue;
-				}
-				// Create a temp list to store one row's data
-				List<String> rowData = new ArrayList<>();
-				// Iterate through the cells in each row
-				for (Cell cells : rows) {
-					switch (cells.getCellType()) {
-					case STRING:
-						// To get string values
-						rowData.add(cells.getStringCellValue().trim());
-						break;
- 
-					case NUMERIC:
-						if (DateUtil.isCellDateFormatted(cells)) {
-							// To get dates
-							rowData.add(cells.getLocalDateTimeCellValue()
-								.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-						} else {
-							// To get numbers
-							rowData.add(String.valueOf(cells.getNumericCellValue()));
-						}
-						break;
-					default:
-						// Add a placeholder string for empty cells
-						rowData.add("-");
-					}
-				}
- 
-				// Add the temp list to the base list
-				data.add(rowData);
- 
-				// Close the file
-				excelFile.close();
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			System.out.println(e.getMessage());
-		}
- 
-		// Return the base list to the DataProvider
-		return data;
-	}
- 
-	@DataProvider(name = "excelDataProvider")
-	public static Object[][] inputData() throws FileNotFoundException, IOException {
-		// Data from the Excel file
-		List<List<String>> inputData = readExcel("src/test/resources/input.xlsx");
- 
-		// Convert the data into a 2D array dataset
-		// This is the dataset that will be passed to the test functions
-		Object[][] data = new Object[inputData.size()][];
-		for (int i = 0; i < inputData.size(); i++) {
-			data[i] = inputData.get(i).toArray();
-		}
- 
-		// Return the dataset
-		return data;
-	}
- 
-	@Test(priority = 1)
-	public void verifyPageTitle() {
-		// Assert the page title
-		assertEquals(driver.getTitle(), "Selenium: Simple Form");
-	}
- 
-	@Test(priority = 2, dataProvider = "excelDataProvider")
-	public void registerTest(String[] rows) throws InterruptedException {
-		// Locate the full name field and add the full from the rows
-		// Find the input fields and enter text
-		driver.findElement(By.id("full-name")).sendKeys(rows[0]);
-		// Enter the email
-		driver.findElement(By.id("email")).sendKeys(rows[1]);
-		// Enter the Date of the event
-		driver.findElement(By.name("event-date")).sendKeys(rows[2]);
-		// Enter additional details
-		driver.findElement(By.id("additional-details")).sendKeys(rows[3]);
- 
-		// Click Submit
-		driver.findElement(By.xpath("//button[text()='Submit']")).click();
- 
-		// Confirm booking
-		String message = driver.findElement(By.id("action-confirmation")).getText();
-		assertEquals(message, "Your event has been scheduled!");
- 
-		// Refresh the page
-		driver.navigate().refresh();
-	}
- 
-	@AfterClass
-	public void tearDown() {
-		// Close the browser
-		driver.quit();
-	}
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.get("https://training-support.net/webelements/simple-form");
+    }
+
+    public static List<List<String>> readExcel(String filePath) {
+
+        List<List<String>> data = new ArrayList<>();
+
+        try (FileInputStream fis = new FileInputStream(filePath);
+             Workbook excelFile = new XSSFWorkbook(fis)) {
+
+            Sheet sheet1 = excelFile.getSheetAt(0);
+
+            for (Row rows : sheet1) {
+
+                if (rows.getRowNum() == 0) {
+                    continue;
+                }
+
+                List<String> rowData = new ArrayList<>();
+
+                for (Cell cell : rows) {
+
+                    switch (cell.getCellType()) {
+
+                    case STRING:
+                        rowData.add(cell.getStringCellValue().trim());
+                        break;
+
+                    case NUMERIC:
+                        if (DateUtil.isCellDateFormatted(cell)) {
+                            rowData.add(cell.getLocalDateTimeCellValue()
+                                    .format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+                        } else {
+                            rowData.add(String.valueOf((long) cell.getNumericCellValue()));
+                        }
+                        break;
+
+                    default:
+                        rowData.add("");
+                    }
+                }
+
+                data.add(rowData);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+
+    @DataProvider(name = "excelDataProvider")
+    public Object[][] inputData() {
+
+        List<List<String>> inputData = readExcel("C://Users//NidhiDhande//Desktop//TestData.xlsx");
+
+        Object[][] data = new Object[inputData.size()][4];
+
+        for (int i = 0; i < inputData.size(); i++) {
+            data[i] = inputData.get(i).toArray();
+        }
+
+        return data;
+    }
+
+    @Test(priority = 1)
+    public void verifyPageTitle() {
+        assertEquals(driver.getTitle(), "Selenium: Simple Form");
+    }
+
+    @Test(priority = 2, dataProvider = "excelDataProvider")
+    public void registerTest(String fullName, String email, String date, String details) {
+
+        driver.findElement(By.id("full-name")).sendKeys(fullName);
+        driver.findElement(By.id("email")).sendKeys(email);
+        driver.findElement(By.name("event-date")).sendKeys(date);
+        driver.findElement(By.id("additional-details")).sendKeys(details);
+
+        // Click Submit
+        driver.findElement(By.xpath("//button[text()='Submit']")).click();
+
+        String message = driver.findElement(By.id("action-confirmation")).getText();
+
+        System.out.println("--------------------------------");
+        System.out.println("Full Name : " + fullName);
+        System.out.println("Email     : " + email);
+        System.out.println("Date      : " + date);
+        System.out.println("Details   : " + details);
+        System.out.println("Confirmation Message : " + message);
+
+        assertEquals(message, "Your event has been scheduled!");
+
+        driver.navigate().refresh();
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
 }
