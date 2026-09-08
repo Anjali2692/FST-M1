b57bd695cb20c889c06c30ac9cdea510621eb9ba
Anjali2692 <anjali.dasari8@gmail.com>
2026-09-08 22:17:07 +0530

    Update Activity1.java

--- Selenium/Activities/Activity1.java
+++ Selenium/Activities/Activity1.java
@@ -1,23 +1,25 @@
+package activity;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Activity1 {
-    public static void main(String[] args) {
-        // Initialize the Firefox Driver
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        // Initialize WebDriver
         WebDriver driver = new FirefoxDriver();
 
-        // Open the page
-        driver.get("https://training-support.net");
+        try {
+            driver.get("https://training-support.net");
+            System.out.println("Home Page Title: " + driver.getTitle());
+            driver.findElement(By.linkText("About Us")).click();
+            System.out.println("About Us Page Title: " + driver.getTitle());
 
-        // Verify the page title
-        System.out.println("Page title is: " + driver.getTitle());
-        // Find the About Us link and click it
-        driver.findElement(By.linkText("About Us")).click();
-        // Print the page title of the About Us page
-        System.out.println("New page title is: " + driver.getTitle());
-
-        // Close the browser
-        driver.quit();
+        } finally {
+            driver.quit();
+        }
     }
 }
