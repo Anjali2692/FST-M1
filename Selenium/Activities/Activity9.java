6a06eb2cd89afe66a5788ae7ab406aadfde594b9
Anjali2692 <anjali.dasari8@gmail.com>
2026-09-08 22:27:54 +0530

    Update Activity9.java

--- Selenium/Activities/Activity9.java
+++ Selenium/Activities/Activity9.java
@@ -1,30 +1,33 @@
+package activity;
+
 import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Activity9 {
-    public static void main(String[] args) {
-        // Create a new instance of the Firefox driver
-        WebDriver driver = new FirefoxDriver();
-        // Create the Wait object
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        // Open the page
-        driver.get("https://www.training-support.net/webelements/dynamic-content");
-        // Print the title of the page
-        System.out.println("Page title: " + driver.getTitle());
+	public static void main(String[] args) {
+ 
+	        WebDriver driver = new FirefoxDriver();
+            driver.get("https://training-support.net/webelements/dynamic-content");
+	        driver.manage().window().maximize();
+            System.out.println("Page Title: " + driver.getTitle());
 
-        // Find the button and click it
-        driver.findElement(By.id("genButton")).click();
-        // Wait for the word to appear
-        if (wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id("word"), "release"))) {
-            // Print the text to console
-            System.out.println("Word found: " + driver.findElement(By.id("word")).getText());
-        }
-        // Close the browser
-        driver.quit();
-    }
-}
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	        WebElement clickButton = driver.findElement(By.xpath("//button[text()='Click me!']"));
+	        clickButton.click();
+
+            wait.until(ExpectedConditions.textToBePresentInElementLocated(By.tagName("body"),"release"));
+            String pageText = driver.findElement(By.tagName("body")).getText();
+            System.out.println("Page Text contains 'release':");
+            System.out.println(pageText);
+
+            driver.quit();
+	        
+	    }
+	}
