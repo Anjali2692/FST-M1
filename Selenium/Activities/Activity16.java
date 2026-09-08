b6b7fdc30560408789e18064ce0c3cdce7fed5f8
Anjali2692 <anjali.dasari8@gmail.com>
2026-09-08 22:20:23 +0530

    Update Activity16.java

--- Selenium/Activities/Activity16.java
+++ Selenium/Activities/Activity16.java
@@ -1,48 +1,40 @@
+package activity;
+
+import java.time.Duration;
 import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Activity16 {
-    public static void main(String[] args) {
-        // Create a new instance of the Firefox driver
-        WebDriver driver = new FirefoxDriver();
 
-        // Open the page
-        driver.get("https://training-support.net/webelements/selects");
-        // Print the title of the page
-        System.out.println("Page title is: " + driver.getTitle());
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+        driver.get(" https://training-support.net/webelements/selects");
+        driver.manage().window().maximize();
+        System.out.println("Page Title: " + driver.getTitle());
 
-        // Find the dropdown
-        WebElement dropdown = driver.findElement(By.cssSelector("select.h-10"));
-        // Pass the WebElement to the Select object
-        Select singleSelect = new Select(dropdown);
-
-        // Select the second option using visible text
+        WebElement singleSelectElement = driver.findElement(By.cssSelector("select.h-10"));
+        Select singleSelect = new Select(singleSelectElement);
         singleSelect.selectByVisibleText("Two");
-        // Print the selected option
-        System.out.println("Second option: " + singleSelect.getFirstSelectedOption().getText());
-
-        // Select the third option using index
+        //singleSelect.selectByIndex(2);
         singleSelect.selectByIndex(3);
-        // Print the selected option
-        System.out.println("Third option: " + singleSelect.getFirstSelectedOption().getText());
-
-        // Select the fourth option using value attribute
         singleSelect.selectByValue("four");
-        // Print the selected option
-        System.out.println("Fourth option: " + singleSelect.getFirstSelectedOption().getText());
 
-        // Print all the options
-        List<WebElement> allOptions = singleSelect.getOptions();
-        System.out.println("Options in the dropdown: ");
-        for(WebElement option : allOptions) {
-            System.out.println(option.getText());
-        }
+        List<WebElement> options = singleSelect.getOptions();
+        System.out.println("\nAvailable Options:");
+        for (WebElement option : options) {
+                System.out.println(option.getText());
+            }
 
-        // Close the browser
-        driver.quit();
+       driver.quit();
     }
 }
