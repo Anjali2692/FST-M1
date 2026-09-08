3c5c40eec4390efbed7163c690ba7ab966bf4855
Anjali2692 <anjali.dasari8@gmail.com>
2026-09-08 22:44:52 +0530

    Update Activity8.java

--- TestNG/Activities/Activity8.java
+++ TestNG/Activities/Activity8.java
@@ -1,24 +1,35 @@
-<?xml version="1.0" encoding="UTF-8" ?>
-<!DOCTYPE suite SYSTEM "https://testng.org/testng-1.0.dtd" >
-<suite name="FSTTests" thread-count="4" parallel="classes">
-    <!-- Comment out groups to run everything -->
-	<groups>
-        <run>
-            <include name="HeaderTests" />
-        </run>
-    </groups>
-    <parameter name="username" value="admin" />
-    <parameter name="password" value="password" />
-    <test name="FSTActivities">
-        <classes>
-            <class name="activities.Activity1" />
-            <class name="activities.Activity2" />
-            <class name="activities.Activity3" />
-            <class name="activities.Activity4_DemoOne" />
-            <class name="activities.Activity4_DemoTwo" />
-            <class name="activities.Activity5" />
-            <class name="activities.Activity6" />
-            <class name="activities.Activity7" />
-        </classes>
-    </test>
-</suite>
+package Activities;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Activity8 {
+	WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeClass
+    public void setUp() {
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get("https://training-support.net/webelements/login-form");
+    }
+    
+    @Test(groups = {"HeaderTests"})
+	public void pageTitleTest() {
+        String title = driver.getTitle();
+        System.out.println("Page Title: " + title);
+        Assert.assertEquals(title,"Selenium: Login Form");
+    }
+    
+    @AfterClass
+    public void tearDown() {
+         driver.quit();
+    }
+}
